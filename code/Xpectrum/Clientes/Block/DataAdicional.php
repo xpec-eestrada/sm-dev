0e6d85e1284d1f5ab6017d702ab9cbdfd0d59905
Erick Estrada <eestrada@xpectrumtech.com>
2017-11-19 01:41:04 +0000

    se convirtio select de genero y marca en label

--- code/Xpectrum/Clientes/Block/DataAdicional.php
+++ code/Xpectrum/Clientes/Block/DataAdicional.php
@@ -5,35 +5,63 @@ namespace Xpectrum\Clientes\Block;
 use Magento\Catalog\Block\Product\Context;
 
 class DataAdicional extends \Magento\Framework\View\Element\Template{
+    protected $registry;
+    protected $product;
 
     public function __construct(
             Context $context,
             array $data = []){
-        // $this->_collectionFactory = $collectionFactory;
-        // $this->_productRepository=$productRepository;
-        // $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        // $storeManager = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
-        // $this->_baseUrl=$storeManager->getStore()->getBaseUrl();
-        // $storeManagerInterface = $context->getStoreManager();
-        // $this->_marca=new Marca($storeManagerInterface->getStore()->getId());
-
+        $this->registry = $context->getRegistry();
         parent::__construct($context, $data);
     }
     public function getInfo(){
         $data          = array();
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance(); // Instance of object manager
-        $resource      = $objectManager->get('Magento\Framework\App\ResourceConnection');
-        $connection    = $resource->getConnection();
-        $tableName     = $resource->getTableName('eav_attribute');
-        $sql           = 'SELECT attribute_id FROM '.$tableName.' WHERE attribute_code=\'genero_sm\' ';
-        $idgenero      = $connection->fetchOne($sql);
-        $sql           = 'SELECT attribute_id FROM '.$tableName.' WHERE attribute_code=\'marca_sm\' ';
-        $idmarca       = $connection->fetchOne($sql);
+        $idgenero      = $this->getIdAttribute('genero_sm');
+        $idmarca       = $this->getIdAttribute('marca_sm');
         $data[]        = array('code'=>'genero_sm','titulo'=>'Genero','id'=>$idgenero);
         $data[]        = array('code'=>'marca_sm','titulo'=>'Marca','id'=>$idmarca);
         return $data;
 
         //return 'id genero: '.$idgenero.' ---  id marca: '.$idmarca;
     }
+    public function getCurrentProduct(){
+        if (is_null($this->product)) {
+            $this->product = $this->registry->registry('product');
+            if (!$this->product->getId()) {
+                throw new LocalizedException(__('Failed to initialize product'));
+            }
+        }
+        return $this->product;
+    }
+    public function getIdAttribute($attribute_code){
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance(); // Instance of object manager
+        $resource      = $objectManager->get('Magento\Framework\App\ResourceConnection');
+        $connection    = $resource->getConnection();
+        $tableName     = $resource->getTableName('eav_attribute');
+        $sql           = 'SELECT attribute_id FROM '.$tableName.' WHERE attribute_code=\''.$attribute_code.'\' ';
+        $id            = $connection->fetchOne($sql);
+        return $id;
+    }
+    public function getAttributesCustom(){
+        $product       = $this->getCurrentProduct();
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance(); // Instance of object manager
+        $resource      = $objectManager->get('Magento\Framework\App\ResourceConnection');
+        $connection    = $resource->getConnection();
+        $tableName     = $resource->getTableName('catalog_product_relation');
+        $sql           = 'SELECT child_id FROM streetmachine_prod.smcatalog_product_relation WHERE parent_id='.$product->getId();
+        $ids           = $connection->fetchAll($sql);
+        $toption       = $resource->getTableName('eav_attribute_option_value');
+        foreach($ids as $id){
+            $childProduct = $objectManager->create('Magento\Catalog\Model\Product')->load($id);
+            if($childProduct->isInStock() && $childProduct->isAvailable() && $childProduct->isSaleable() ){
+                echo '<pre>'.$childProduct->getData('talla_sm').'</pre>';
+                $sql = 'SELECT value FROM '.$toption.' WHERE option_id='.$childProduct->getData('talla_sm');
+
+            }
+
+            
+        }
+    }
+    //SELECT value FROM streetmachine_prod.smeav_attribute_option_value ov WHERE option_id=13138;
 
 }
--- code/Xpectrum/Clientes/view/frontend/web/js/xpec_selectProducts.js
+++ code/Xpectrum/Clientes/view/frontend/web/js/xpec_selectProducts.js
@@ -2,34 +2,38 @@ require([
     "jquery"
     ],function($){
         window.fnload=function(){
+            
             var ids=[];
             var labels=[];
-            if($('.xpec-select').length){
-                $.each($('.xpec-select'),function(index,element){
-                    ids[index]=$(this).val();
-                    labels[index]=$(this).attr('data-title');
-                });
-            }
-            
+            // if($('.xpec-select').length){
+            //     $.each($('.xpec-select'),function(index,element){
+            //         ids[index]=$(this).val();
+            //         labels[index]=$(this).attr('data-title');
+            //     });
+            // }
             if(window.swloadselect){
                 window.cont++;
             }
-            if(window.cont<15){
-                $(".content-xpecselect").remove();
-                $.each(ids,function(index,value){
-                    if($("select[name='super_attribute["+value+"]']").length){
-                        window.swloadselect=true;
-                        if($("select[name='super_attribute["+value+"]'] option").eq(1).length){
-                            $("select[name='super_attribute["+value+"]']").val($("select[name='super_attribute["+value+"]'] option").eq(1).attr('value'));
-                            $('<div class="content-xpecselect"><div class="cont-xpec-lab" ><span class="label">'+labels[index]+'</span></div><div class="cont-xpec-val"><span class="value">'+$("select[name='super_attribute["+value+"]'] option").eq(1).text()+'</span></div></div>').insertBefore('.product-info-main .product-add-form');
-                        }else{
-                            $('<div class="content-xpecselect"><div class="cont-xpec-lab" ><span class="label">'+labels[index]+'</span></div><div class="cont-xpec-val"><span class="value">No tiene '+labels[index]+'</span></div></div>').insertBefore('.product-info-main .product-add-form');
+            if(window.cont<10){
+                if( $('.field.configurable').length ){
+                    window.swloadselect=true;
+                    $.each($('.field.configurable'),function(index,element){
+                        var $select=$(this).find('select');
+                        if( $($select).find('option').eq(1).attr('value') != undefined ){
+                            $($select).val($($select).find('option').eq(1).attr('value'));
+                            $($select).trigger('change');
                         }
-                        if(window.cont==0){
-                            $("select[name='super_attribute["+value+"]']").parents('.field.configurable').addClass('xpec-hidden');;
-                        }
-                    }
-                });
+                    });
+                }
+                $('.fiedxpec').remove();
+                if($('.xpec-select').length){
+                    $.each($('.xpec-select'),function(index,element){
+                        var valor=$("select[name='super_attribute["+$(this).val()+"]']").val();
+                        var texto=$("select[name='super_attribute["+$(this).val()+"]'] option:selected").text();
+                        var valor=$("select[name='super_attribute["+$(this).val()+"]']").parents('.field.configurable').css({'display':'none'});
+                        $('<div class="fiedxpec content-xpecgenero"><div class="cont-xpec-lab" ><span class="label">'+$(this).attr('data-title')+'</span></div><div class="cont-xpec-val"><span class="value">'  +texto+  '</span></div></div>').insertBefore('.product-info-main .product-add-form');
+                    });
+                }
                 
             }
         }
@@ -39,8 +43,6 @@ require([
             window.fnload();
             $(window).scroll(function(e){
                 window.fnload();
-                //loadselect();
-                
             });
             $('body').mouseover(function(){
                 window.fnload();
